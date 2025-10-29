e56703060f84fdb06e49b5d59ae89d274e6e4fb9
Abdoulie Faal <faalabdoulie@gmail.com>
2025-10-29 13:02:14 +0000

    Add Practical5 Codes

---
+++ practical5/Arctan.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<math.h>
+
+double arctanh1(const double x, const double delta);
+double arctanh2(const double x);
+
+int main(){
+
+  double delta,x;
+  printf("Enter delta\n");
+  scanf("%lf",&delta);
+  
+  int length=1000;
+  double tan1[length];
+  double tan2[length];
+
+  x=-0.9;
+  int j=0; //array index
+
+  while(x<=0.9 && j<length){
+    tan1[j] = arctanh1(x, delta);
+    tan2[j] = arctanh2(x);
+    printf("Difference at %lf is %.10lf\n", x, fabs(tan1[j]-tan2[j]));
+    j++;
+    x=x+0.1;
+  }
+
+  return 0;
+}
+
+//Inverse hyperbolic tangent approximation
+  double arctanh1(const double x, const double delta){
+  double arcTan = 0; //approximated
+  double elem,val;
+  int n=0;
+
+  do{
+    val=2*n+1;
+    elem=pow(x,val)/val;
+    arcTan += elem; //arcTan=arctTan+elem;
+    n++;
+  
+  } while(fabs(elem) >= delta );
+
+  return arcTan;
+}
+
+//Real value of arctanh
+  double arctanh2(const double x){
+  return ((log(1+x)-log(1-x))/2);
+}
---
+++ practical5/fibonacci.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Function prototype
+//Input arguments: Fn-1 nd Fn-2
+//On exit, Fn and Fn-1
+void fibonacci(int *a, int *b);
+
+int main()
+{
+  int n, i;
+  int f1=1, f0=0;
+
+  //Get user to enter a number
+  printf("Enter positive integer\n");
+  scanf("%d",&n);
+
+  //Check if the number is positive
+  if(n<1){
+    printf("The number is not positive\n");
+    exit(1);//stops program execution
+  }
+
+  printf("Fibonacci sequence is: \n");
+  printf("%d, %d", f0,f1);
+
+  for(i=2;i<=n;i++){
+    fibonacci(&f1,&f0);
+    printf("%d ", f1);
+    if ((i+1)%10 ==0)
+      printf("\n");
+  }
+
+  return 0;
+}
+
+void fibonacci(int *a, int *b){
+
+  int next;
+  //a=Fn-1, b=Fn-2
+  next=*a+*b;
+
+  //On exit *a=fn, *b=fn-1
+  *b=*a;
+  *a=next;
+
+}
+
+
